ecc8b1f1980a9bbd6fbe8a0e5955b281b6a3ca3e
DatDonathan <jojo08082003@gmail.com>
2020-10-05 13:14:24 +0200

    Added second floor in cave

--- src/at/jojokobi/minigamesplugin/stuctures/cave/CaveStructure.java
+++ src/at/jojokobi/minigamesplugin/stuctures/cave/CaveStructure.java
@@ -5,6 +5,7 @@ import java.util.Random;
 import org.bukkit.Location;
 import org.bukkit.Material;
 
+import at.jojokobi.mcutil.generation.BasicGenUtil;
 import at.jojokobi.mcutil.generation.population.TunnelPathGenerator;
 import at.jojokobi.mcutil.generation.population.VillageSpreader;
 import at.jojokobi.minigamesplugin.stuctures.GameStructure;
@@ -12,23 +13,40 @@ import at.jojokobi.minigamesplugin.stuctures.GameStructure;
 
 public class CaveStructure implements GameStructure {
 	
-	private VillageSpreader spreader;
+	private VillageSpreader upperSpreader;
+	private VillageSpreader lowerSpreader;
 	
 	public CaveStructure(int width, int height) {
-		spreader = new VillageSpreader(new LootRoom(), new LootRoom(), new LootRoom(), new KitRoom(), new KitRoom(), new AnvilRoom(), new MeetingRoom(), new MeetingRoom());
-		spreader.setStepMultiplier(0.5f);
-		spreader.setForceHeight(true);
-		spreader.setBlockFunction(b -> Material.AIR);
-		spreader.setPathGenerator(new TunnelPathGenerator());
-		spreader.setUnitHeight(16);
-		spreader.setUnitHeight(16);
-		spreader.setWidth(width/16);
-		spreader.setHeight(height/16);
+		{
+			lowerSpreader = new VillageSpreader(new LootRoom(), new LootRoom(), new LootRoom(), new KitRoom(), new KitRoom(), new AnvilRoom(), new MeetingRoom(), new MeetingRoom());
+			lowerSpreader.setStepMultiplier(0.5f);
+			lowerSpreader.setForceHeight(true);
+			lowerSpreader.setBlockFunction(b -> Material.AIR);
+			lowerSpreader.setPathGenerator(new TunnelPathGenerator());
+			lowerSpreader.setUnitHeight(16);
+			lowerSpreader.setUnitHeight(16);
+			lowerSpreader.setWidth(width/16);
+			lowerSpreader.setHeight(height/16);
+		}
+		{
+			upperSpreader = new VillageSpreader(new LootRoom(), new LootRoom(), new KitRoom(), new KitRoom(), new KitRoom(), new MeetingRoom(), new MeetingRoom());
+			upperSpreader.setStepMultiplier(0.5f);
+			upperSpreader.setForceHeight(true);
+			upperSpreader.setBlockFunction(b -> Material.AIR);
+			upperSpreader.setPathGenerator(new TunnelPathGenerator());
+			upperSpreader.setUnitHeight(16);
+			upperSpreader.setUnitHeight(16);
+			upperSpreader.setWidth(width/16);
+			upperSpreader.setHeight(height/16);
+		}
 	}
 
 	@Override
 	public void generate(Location loc, Random random) {
-		spreader.generateVillage(random, random.nextLong(), loc);
+		Location centerPlace = loc.clone().add(lowerSpreader.getWidth()/2 * lowerSpreader.getUnitWidth() + lowerSpreader.getUnitWidth()/2, 0, lowerSpreader.getHeight()/2 * lowerSpreader.getUnitHeight() + lowerSpreader.getUnitHeight()/2);
+		BasicGenUtil.generateCube(centerPlace.add(-2, 0, -2), Material.AIR, 4, 4, 4);
+		lowerSpreader.generateVillage(random, random.nextLong(), loc);
+		upperSpreader.generateVillage(random, random.nextLong(), loc.clone().add(0, 10, 0));
 	}
 
 }
