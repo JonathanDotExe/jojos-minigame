db40b661c9c6b67c46a43c6aed06802a2d294d7f
DatDonathan <jojo08082003@gmail.com>
2020-12-28 20:40:24 +0100

    Added specialized player kits for sky siege

--- src/at/jojokobi/minigamesplugin/minigames/SkySiegeMinigame.java
+++ src/at/jojokobi/minigamesplugin/minigames/SkySiegeMinigame.java
@@ -1,5 +1,6 @@
 package at.jojokobi.minigamesplugin.minigames;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -7,11 +8,13 @@ import java.util.Random;
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scoreboard.Scoreboard;
 
@@ -24,7 +27,6 @@ import at.jojokobi.minigamesplugin.items.TNTEggComponent;
 import at.jojokobi.minigamesplugin.items.UnstableTNTComponent;
 import at.jojokobi.minigamesplugin.items.WitherSkullGunComponent;
 import at.jojokobi.minigamesplugin.kits.PlayerKit;
-import at.jojokobi.minigamesplugin.kits.PlayerKits;
 import at.jojokobi.minigamesplugin.maps.MapGenerator;
 import at.jojokobi.minigamesplugin.minigames.components.ClimbComponent;
 import at.jojokobi.minigamesplugin.minigames.components.StrengthComponent;
@@ -40,10 +42,48 @@ public class SkySiegeMinigame extends BaseMinigame{
 	private int maxWaitTime = 20 * 20;
 	
 	private GlobalScore<Integer> timerScore;
+	
+	private List<PlayerKit> kits = new ArrayList<PlayerKit>();
 
 	
 	public SkySiegeMinigame(MapGenerator generator, MapGenerator lobbyGenerator, Area gameArea) {
 		super(generator, lobbyGenerator, gameArea);
+		
+		kits.add(new PlayerKit(
+			new ItemStack(Material.IRON_SWORD),
+			new ItemStack(Material.IRON_PICKAXE),
+			new ItemStack(Material.FISHING_ROD),
+			new ItemStack(Material.IRON_HELMET),
+			new ItemStack(Material.IRON_CHESTPLATE),
+			new ItemStack(Material.IRON_LEGGINGS),
+			new ItemStack(Material.IRON_BOOTS),
+			new ItemStack(Material.COOKED_BEEF, 16),
+			new ItemStack(Material.SHIELD),
+			new ItemStack(Material.GOLDEN_APPLE, 5),
+			new ItemStack(Material.WATER_BUCKET),
+			new ItemStack(Material.LAVA_BUCKET),
+			new ItemStack(Material.OAK_PLANKS, 64),
+			new ItemStack(Material.BIRCH_PLANKS, 64),
+			new ItemStack(Material.ENDER_PEARL, 3)
+		));
+		
+		kits.add(new PlayerKit(
+			new ItemStack(Material.STONE_SWORD),
+			new ItemStack(Material.IRON_PICKAXE),
+			new ItemStack(Material.BOW),
+			new ItemStack(Material.GOLDEN_HELMET),
+			new ItemStack(Material.GOLDEN_CHESTPLATE),
+			new ItemStack(Material.GOLDEN_LEGGINGS),
+			new ItemStack(Material.GOLDEN_BOOTS),
+			new ItemStack(Material.COOKED_BEEF, 32),
+			new ItemStack(Material.COOKED_MUTTON, 32),
+			new ItemStack(Material.GOLDEN_APPLE, 4),
+			new ItemStack(Material.SNOWBALL, 16),
+			new ItemStack(Material.ARROW, 42),
+			new ItemStack(Material.STONE, 64),
+			new ItemStack(Material.STONE, 64),
+			new ItemStack(Material.ENDER_PEARL, 3)
+		));
 	}
 	
 	@Override
@@ -75,7 +115,7 @@ public class SkySiegeMinigame extends BaseMinigame{
 			//Display role
 			player.sendTitle("The Sky Siege starts now!", "Kill everyone and survive!", 20, 80, 20);
 			//Give kit
-			PlayerKit kit = PlayerKits.KITS.get(random.nextInt(PlayerKits.KITS.size()));
+			PlayerKit kit = kits.get(random.nextInt(kits.size()));
 			kit.give(player.getInventory());
 		}
 	}
@@ -117,16 +157,16 @@ public class SkySiegeMinigame extends BaseMinigame{
 	protected void spreadPlayers (List<Player> players, Area gameArea) {
 		Random random = new Random();
 		for (Player player : players) {
-			Location loc = gameArea.getPos().clone().add(random.nextInt((int) gameArea.getWidth()/ISLAND_GRID_STEP) * ISLAND_GRID_STEP, gameArea.getPos().getWorld().getMaxHeight() - gameArea.getPos().getY(), random.nextInt((int) gameArea.getLength()/ISLAND_GRID_STEP) * ISLAND_GRID_STEP);
+			Location loc = gameArea.getPos().clone().add(random.nextInt((int) gameArea.getWidth()/ISLAND_GRID_STEP) * ISLAND_GRID_STEP, 0, random.nextInt((int) gameArea.getLength()/ISLAND_GRID_STEP) * ISLAND_GRID_STEP);
 			loc.setY(loc.getWorld().getHighestBlockYAt(loc));
-			player.teleport(player);
+			player.teleport(loc);
 		}
 	}
 	
 	@EventHandler
 	public void onEntityDamage (EntityDamageEvent event) {
 		if (getGameArea().getPos().getWorld().getPlayers().contains(event.getEntity())) {
-			//Prevent damage in protection time
+			//Prevent damage in lobby
 			if (!isRunning()) {
 				event.setDamage(0);
 			}
@@ -178,6 +218,7 @@ public class SkySiegeMinigame extends BaseMinigame{
 	}
 	
 	@Override
+	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent event) {
 		//let player die when they fall down
 		//TODO sideeffects in the future maybe
